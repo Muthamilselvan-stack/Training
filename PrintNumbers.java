03d939d8e6e749f654cdec5f0e4e14adab93d301
Muthamilselvan-stack <muthamilselvan596@gmail.com>
2025-02-18 12:24:05 +0530

    Add files via upload

---
+++ PrintNumbers.java
@@ -0,0 +1,15 @@
+public class PrintNumbers {
+    public static void main(String[] args) {
+        int num1 = 5;
+        int num2 = 10;
+        System.out.print(num1);
+        System.out.print("");
+        System.out.print(num2);
+            if (num1>num2){
+                   System.out.println("greatest:"+num1);
+       }
+            else{
+           System.out.println("  greatest number:"+num2);
+    }
+  }
+}
